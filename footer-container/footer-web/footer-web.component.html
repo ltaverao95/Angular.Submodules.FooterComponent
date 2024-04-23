1488c919a5ff0b23879d455857482cbe4d3485fd
Luis Felipe Tavera <luis.tavera@newshore.es>
2024-04-23 06:36:44 -0500

    included footer

---
+++ footer-container/footer-web/footer-web.component.html
@@ -0,0 +1,1 @@
+<span>FOOTER</span>
---
+++ footer-container/footer-web/footer-web.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'footer-web',
+  standalone: true,
+  templateUrl: './footer-web.component.html'
+})
+export class FooterWebComponent {
+  
+}
