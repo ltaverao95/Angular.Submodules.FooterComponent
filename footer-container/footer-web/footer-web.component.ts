ac4cd1ba7032b7d2999f7ee16e0abca62f965a81
natalia.tavera <natalia.tavera@flyrlabs.com>
2024-04-24 09:50:41 -0500

    Copyright and social links added

--- footer-container/footer-web/footer-web.component.ts
+++ footer-container/footer-web/footer-web.component.ts
@@ -3,7 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'footer-web',
   standalone: true,
-  templateUrl: './footer-web.component.html'
+  templateUrl: './footer-web.component.html',
+  styles: [`
+  .footer{
+    &__text{
+      color: aqua;
+    }
+    &__link{
+      color: orange;
+    }
+  } 
+`]
 })
 export class FooterWebComponent {
   
